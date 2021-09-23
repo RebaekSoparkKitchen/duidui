68b3f101216ab1adae3d9ce0767081121e8b2ad0
FlyingRedPig <zinan.xu@sap.com>
2021-09-23 11:49:58 +0800

    develop: paragraph update paragraphText

---
+++ src/paragraph.ts
@@ -0,0 +1,55 @@
+/**
+ * the main implementation of paragraph split, only work on a single lineBreak
+ * @param text target string
+ * @param lineBreak the lineBreak type, say <br>, \n
+ * @returns a string[] which each element represents a paragraph
+ */
+function _norm(text: string, lineBreak: string) {
+  let paragraphs = text.split(lineBreak);
+  paragraphs = paragraphs
+    .map((value) => value.trim())
+    .filter((value) => !!value);
+  return paragraphs;
+}
+/**
+ * hard code all lineBreak in the function, and get the paragraph list
+ * @param text target string
+ * @returns a string[] which each element represents a paragraph
+ */
+function norm(text: string) {
+  let str = text;
+  let paragraphs: string[];
+  const lineBreaks = [`\n`, `<br>`, `<br/>`, `<br />`];
+  paragraphs = [str];
+  // recursive call norm on each paragraph list and finally flat it.
+  lineBreaks.forEach((lineBreak) => {
+    paragraphs = paragraphs.map((p) => _norm(p, lineBreak)).flat();
+  });
+  return paragraphs;
+}
+/**
+ *
+ * @param text target string
+ * @param output output type, determined it is /n or <br>
+ * @param margin single row margin or double row margin
+ * @returns
+ */
+function paragraphText(
+  text: string,
+  output: 'html' | 'plain',
+  margin: 'single' | 'double'
+) {
+  let lineBreak: string;
+  switch (output) {
+    case 'plain':
+      lineBreak = margin === 'single' ? '\n' : '\n\n';
+      return norm(text).join(lineBreak);
+    case 'html':
+      lineBreak = margin === 'single' ? '<br>' : '<br><br>';
+      return norm(text).join(lineBreak);
+    default:
+      return text;
+  }
+}
+
+export { norm, paragraphText };
