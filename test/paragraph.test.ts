33d0de3d6fe4448d4a1799ad6595ecc8ed6d6533
FlyingRedPig <zinan.xu@sap.com>
2021-09-23 11:55:34 +0800

    develop: update test cases

---
+++ test/paragraph.test.ts
@@ -0,0 +1,50 @@
+import chai = require('chai');
+const assert = chai.assert;
+import { norm, paragraphText } from '../src/paragraph';
+
+describe('paragraph', () => {
+  describe('norm()', () => {
+    it('should return paragraph list', () => {
+      const sample = `
+			
+			william
+
+			luo<br>luo
+
+			flying<br/>pig
+			`;
+      const expected = ['william', 'luo', 'luo', 'flying', 'pig'];
+      const res = norm(sample);
+
+      assert.deepEqual(expected, res);
+    });
+    it('should not split paragraphs', () => {
+      const sample = `   如此这样三十年 `;
+      const expected = [`如此这样三十年`];
+      const res = norm(sample);
+      assert.deepEqual(expected, res);
+    });
+  });
+  describe('paragraphText()', () => {
+    const sample = `白日依山尽
+		  
+		 黄河入海流
+		 
+		 `;
+    it('should return plain text', () => {
+      const expected = '白日依山尽\n黄河入海流';
+      const res = paragraphText(sample, 'plain', 'single');
+      assert.strictEqual(res, expected);
+    });
+    it('should return html text', () => {
+      const expected = '白日依山尽<br>黄河入海流';
+      const res = paragraphText(sample, 'html', 'single');
+      assert.strictEqual(res, expected);
+    });
+    it('should return double lineBreaks', () => {
+      const expected = '白日依山尽<br><br>黄河入海流';
+      const res = paragraphText(sample, 'html', 'double');
+      assert.strictEqual(res, expected);
+    });
+  });
+});
