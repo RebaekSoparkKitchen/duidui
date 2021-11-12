db5ec154ca4898883215bf1ce35a9e27c9f18fb0
FlyingRedPig <zinan.xu@sap.com>
2021-11-12 16:25:47 +0800

    test: add some real world cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -26,11 +26,22 @@ describe('index.ts', () => {
       const expected = '那一年我二十一岁，在我一生的<b> gold</b> 时代。';
       assert.equal(res.data, expected);
     });
-    it('shoule avoid add any space', () => {
+    it('shoule avoid any space from real case1', () => {
       const sample = `届时，<b>我们将全网直播本届峰会上午的「主旨演讲」</b>`;
       const res = duidui.proofread(sample);
       const expected = `届时，<b>我们将全网直播本届峰会上午的「主旨演讲」</b>`;
       assert.equal(res.data, expected);
+
+      const sample1 = `中国在联合国大会上向全世界公布了关于<b>「双碳」</b>的重大承诺`;
+      const res1 = duidui.proofread(sample1);
+      const expected1 = `中国在联合国大会上向全世界公布了关于<b>「双碳」</b>的重大承诺`;
+      assert.strictEqual(res1.data, expected1);
+    });
+    it('should avoid any space from real case 2', () => {
+      const sample = `10:00—11:30 am，<b>融通・创拓 | SAP 离散制造行业云上大讲堂 BTP 云专场</b>正式开启，`;
+      const expected = `10:00—11:30 am，<b>融通・创拓 | SAP 离散制造行业云上大讲堂 BTP 云专场</b>正式开启，`;
+      const res = duidui.proofread(sample, { paragraph: false });
+      assert.strictEqual(res.data, expected);
     });
   });
 });
